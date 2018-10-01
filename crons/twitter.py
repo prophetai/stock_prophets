53f8872d8491a94664b7af99208ac4ceaadb814c
Dudley Esteban Díaz Suárez <deds15@gmail.com>
2018-10-01 01:51:02 -0500

    se hacen modificaciones para cargar los tweets

--- crons/twitter.py
+++ crons/twitter.py
@@ -7,6 +7,7 @@ import json
 import pandas as pd
 import logging
 import twint
+import datetime
 from utils.extract import db_connection, download_data
 
 
@@ -23,14 +24,17 @@ def search_tweets(cuenta):
     c = twint.Config()
     c.Username = cuenta.replace('@','')
     c.Pandas = True
-
-    twint.run.Search(c)
+    c.Limit = 10
+    try:
+        twint.run.Search(c)
+    except asyncio.TimeoutError as error:
+        logging.error('Error en la búsqueda de tweets: %s' % (error))
 
     df = twint.storage.panda.Tweets_df
 
     return df
 
-def load_tweets(df, creds):
+def load_tweets(DF, creds):
     """
     Carga los tweets desde un dataframe a una base de datos
 
@@ -39,14 +43,28 @@ def load_tweets(df, creds):
         creds(dict): Diccionario con las credenciales de la base de datos
     """
     conn = db_connection(creds)
+    df = DF.copy()
+    logging.warning('COLUMNAS: %s' , (df.columns))
+    new_order = ['id', 'user_id', 'date', 'timezone', 'location', 'username', 'tweet', 'hashtags', 'link', 'retweet', 'user_rt', 'mentions']
+    df = df[new_order]
 
-    date_time = str(datetime.now())
+    date_time = str(datetime.datetime.now())
 
     lista_tweets = df.values.tolist()
-    lista_tweets = [tuple(tweet.append(date_time)) for tweet in lista_tweets]
+    lista_tweets = ['('+str(tweet).replace("['","'").replace("']","'")+')' for tweet in lista_tweets]
+
     print('se van a guardar {} tweets de la cuenta: {}'.format(len(lista_tweets), lista_tweets[0][12]))
 
-    data_ready = str(lista_tweets[1:-1])
+    data_ready = []
+
+    for tweet in lista_tweets:
+        element_ready = str(element for element in tweet) # WARNING: hay que pensar en los elementos que son listas
+        data_ready.append(element_ready)
+
+        data_ready = data_ready + " )"
+    data_ready = data_ready.replace('[','').replace(']','').replace('"','').replace(', ,',",'',").replace('None',"''")
+
+    logging.warning(data_ready)
 
     query= """INSERT INTO tweets VALUES {};""".format(data_ready)
 
@@ -66,7 +84,7 @@ def main():
     lista_cuentas = pd.read_csv('Cuentas BMBV.csv')['Cuentas']
 
     for cuenta in lista_cuentas:
-        df = obtiene_todos_tweets(cuenta)
+        df = search_tweets(cuenta)
 
         try:
             load_tweets(df, creds)
