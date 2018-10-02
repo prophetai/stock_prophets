ed58af17a7738b9be8edcb1d637f3bbcdcca5e82
Dudley Esteban Díaz Suárez <deds15@gmail.com>
2018-10-01 21:27:14 -0500

    Se mejora tweeter y se hacen cambios en obtención de datos de stocks

--- crons/twitter.py
+++ crons/twitter.py
@@ -24,7 +24,7 @@ def search_tweets(cuenta):
     c = twint.Config()
     c.Username = cuenta.replace('@','')
     c.Pandas = True
-    c.Limit = 10
+    #c.Limit = 10
     try:
         twint.run.Search(c)
     except asyncio.TimeoutError as error:
@@ -44,34 +44,40 @@ def load_tweets(DF, creds):
     """
     conn = db_connection(creds)
     df = DF.copy()
-    logging.warning('COLUMNAS: %s' , (df.columns))
+
     new_order = ['id', 'user_id', 'date', 'timezone', 'location', 'username', 'tweet', 'hashtags', 'link', 'retweet', 'user_rt', 'mentions']
     df = df[new_order]
-
+    df['hashtags'].replace('[','',inplace=True)
+    df['hashtags'].replace(']','',inplace=True)
     date_time = str(datetime.datetime.now())
 
     lista_tweets = df.values.tolist()
-    lista_tweets = ['('+str(tweet).replace("['","'").replace("']","'")+')' for tweet in lista_tweets]
+    #lista_tweets = [str(tweet) for element in tweet for tweet in lista_tweets ]
 
-    print('se van a guardar {} tweets de la cuenta: {}'.format(len(lista_tweets), lista_tweets[0][12]))
-
-    data_ready = []
+    data_ready = ''
 
     for tweet in lista_tweets:
-        element_ready = str(element for element in tweet) # WARNING: hay que pensar en los elementos que son listas
-        data_ready.append(element_ready)
+        tweet_str = []
+        for element in tweet:
+            print("element: %s" % (element))
+            element = str(element).replace("'", '')
+            transform = "" + str(element).replace("['", '[').replace("']",']')
+            #transform = transform.replace('"[#',"'[#")
+            print("transform: %s" % (transform))
+            tweet_str.append(transform)
 
-        data_ready = data_ready + " )"
-    data_ready = data_ready.replace('[','').replace(']','').replace('"','').replace(', ,',",'',").replace('None',"''")
+        data_ready += "(" + str(tweet_str)[1:-1] + ")"
 
-    logging.warning(data_ready)
+    data_ready = data_ready.replace(")(",'), (')
+    print('Se van a guardar {}'.format(data_ready))
 
-    query= """INSERT INTO tweets VALUES {};""".format(data_ready)
 
-    try:
-        download_data(conn,query)
-    except Exception as e:
-        logging.error('Error al insertar en la base: %s' % (e))
+    query = """INSERT INTO tweets VALUES {} ON CONFLICT (id) DO NOTHING;""".format(data_ready)
+
+    #try:
+    download_data(conn,query)
+    #except Exception as e:
+    #    logging.error('Error al insertar en la base: %s' % (e))
 
 def main():
     """
@@ -86,10 +92,10 @@ def main():
     for cuenta in lista_cuentas:
         df = search_tweets(cuenta)
 
-        try:
-            load_tweets(df, creds)
-        except Exception as e:
-            logging.error('Error al insertar en la base %s' % (e))
+        #try:
+        load_tweets(df, creds)
+        #except Exception as e:
+        #    logging.error('Error al insertar en la base %s' % (e))
 
 if __name__ == "__main__":
     version = ".".join(str(v) for v in sys.version_info[:2])
